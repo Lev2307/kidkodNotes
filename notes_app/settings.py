e3c2d525e74a8d7522a017449e30fb242a78900f
Lev2307 <Abrosimow1@mail.ru>
2022-05-06 20:03:19 +0300

    upd settings

--- notes_app/settings.py
+++ notes_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1i&u45y53mu_!^m3mmdwzs5v)v@h7+mpm)!^vs!q^no6#eqpob
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['powerful-tundra-35946.herokuapp.com']
 
 
 # Application definition
